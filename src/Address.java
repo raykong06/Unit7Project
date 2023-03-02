e3093109650c0cb1313ead6891effe170bea4138
BT_4N2_12 <rkong2588@bths.edu>
2023-03-02 12:33:06 -0500

    added instance variables and started constructor for Address class

--- src/Address.java
+++ src/Address.java
@@ -1,2 +1,13 @@
 public class Address {
+    private String houseNum;
+    private String street;
+    private String aptNum;
+    private String city;
+    private String state;
+    private int[] zipCode;
+
+    public Address(String houseNum, String street, String aptNum, String city, String state)
+    {
+
+    }
 }
